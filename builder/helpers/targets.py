fb33c4999c113b39ff81f0bc29055ef438a7e2dc
NigelB <nigel.blair@gmail.com>
2023-07-23 01:19:57 +1000

    Update for BLE OTA Image creation.

--- builder/helpers/targets.py
+++ builder/helpers/targets.py
@@ -12,8 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+import shutil
 
-from SCons.Script import DefaultEnvironment, Import, BUILD_TARGETS
+from SCons.Script import DefaultEnvironment, Import, BUILD_TARGETS, ARGUMENTS
 from os.path import join
 import sys
 
@@ -89,7 +91,6 @@ def add_jlink_rtt(env):
     rtt_actions = [env.VerboseAction("$APOLLO3_RTT_COMMAND", "Start the SEGGER Jlink RTT program."),]
     env.AddPlatformTarget("jlink_rtt", None, rtt_actions, "JLink RTT", "Start the SEGGER Jlink RTT program.")
 
-
 def add_ota_image(env):
     try:
         from Crypto.Cipher import AES
@@ -100,20 +101,72 @@ def add_ota_image(env):
     platform_apollo3blue = env.PioPlatform()
     FRAMEWORK_DIR = platform_apollo3blue.get_package_dir("framework-ambiqsuitesdkapollo3-sfe")
     env.Replace(
-        APOLLO3_SECURITY_KEYS=board.get("board_build.ota.keys_dir", join("$PROJECT_DIR", "keys")),
+        APOLLO3_SECURITY_KEYS=board.get("build.ota.keys_dir", join("$PROJECT_DIR", "keys")),
         APOLLO3_OTA_IMAGE_STAGE1=join("$BUILD_DIR", "ota_image_stage1"),
         APOLLO3_OTA_IMAGE_STAGE1_BIN=join("$BUILD_DIR", "ota_image_stage1.bin"),
-        APOLLO3_OTA_IMAGE=join("$BUILD_DIR", "ota_image.bin"),
-        APOLLO3_CUSTOM_IMAGE_PROGRAM=join(FRAMEWORK_DIR, "tools", "apollo3_scripts", "create_cust_image_blob.py"),
+        APOLLO3_OTA_IMAGE=join("$BUILD_DIR", board.get("build.ota.image_name", "ota_image")),
+        APOLLO3_SCRIPTS_DIR=join(FRAMEWORK_DIR, "tools", "apollo3_scripts"),
+        APOLLO3_CUSTOM_IMAGE_PROGRAM=join("$APOLLO3_SCRIPTS_DIR", "create_cust_image_blob.py"),
         APOLLO3_OTA_BINARY_PROGRAM=join(FRAMEWORK_DIR, "tools", "apollo3_amota", "scripts", "ota_binary_converter.py"),
         APOLLO3_CUSTOM_IMAGE_COMMAND="PYTHONPATH=$APOLLO3_SECURITY_KEYS $PYTHONEXE $APOLLO3_CUSTOM_IMAGE_PROGRAM --bin $SOURCE --load-address $UPLOAD_ADDRESS --magic-num 0xcb --version 0x0 -o $APOLLO3_OTA_IMAGE_STAGE1",
-        APOLLO3_OTA_IMAGE_COMMAND="$PYTHONEXE $APOLLO3_OTA_BINARY_PROGRAM --appbin $APOLLO3_OTA_IMAGE_STAGE1_BIN -o $APOLLO3_OTA_IMAGE",
+        APOLLO3_OTA_IMAGE_COMMAND="$PYTHONEXE $APOLLO3_OTA_BINARY_PROGRAM --appbin $APOLLO3_OTA_IMAGE_STAGE1_BIN --load-address $UPLOAD_ADDRESS -o $APOLLO3_OTA_IMAGE",
     )
+
+    def find_keys(target=None, source=None, env=None):
+        """
+        Ensure that we have a 'keys_info.py' file.
+        :param target:
+        :param source:
+        :param env:
+        :return:
+        """
+        key_dir = env.subst("$APOLLO3_SECURITY_KEYS")
+        keys_file = join(key_dir, "keys_info.py")
+        if not os.path.exists(keys_file):
+            print("{} is missing, you can create it with the 'create_ambiq_keys' target.".format(keys_file), file=sys.stderr)
+            sys.exit(1)
+
     ota_image_actions = [
+        env.VerboseAction(find_keys, "Find Security Keys"),
         env.VerboseAction("$APOLLO3_CUSTOM_IMAGE_COMMAND", "Custom Image."),
         env.VerboseAction("$APOLLO3_OTA_IMAGE_COMMAND", "OTA Image."),
     ]
-    env.AddPlatformTarget("ble_ota_image", "$BUILD_DIR/firmware.bin", ota_image_actions, "BLE OTA", "Create a program image to be used by the OTA updater.")
+    env.AddPlatformTarget("ble_ota_image", "$BUILD_DIR/firmware.bin", ota_image_actions, "BLE OTA Image", "Create a program image to be used by the OTA updater.")
+
+
+def _create_keys(target=None, source=None, env=None):
+    """
+    This function is the action run by the create_ambiq_keys target. It copies the example keys from the
+    AmbiqSuiteSDK into a directory specified by the 'build.ota.keys_dir' option, defaulting to 'keys'.
+    :param target:
+    :param source:
+    :param env:
+    :return:
+    """
+    is_verbose = int(ARGUMENTS.get("PIOVERBOSE", 0)) == 1
+    key_dir = env.subst("$APOLLO3_SECURITY_KEYS")
+    if not os.path.exists(key_dir):
+        if is_verbose:
+            print("Creating keys directory: {}".format(key_dir), file=sys.stderr)
+        os.makedirs(key_dir)
+    keys_file = join(key_dir, "keys_info.py")
+    example_keys_file = join(env.subst("$APOLLO3_SCRIPTS_DIR"), "keys_info0.py")
+    if not os.path.exists(keys_file):
+        if is_verbose:
+            print("Copying {} to {}".format(example_keys_file, keys_file), file=sys.stderr)
+        shutil.copyfile(example_keys_file, keys_file)
+
+def add_create_default_ota_security_keys(env):
+    """
+    Adds the create_ambiq_keys target
+    :param env:
+    :return:
+    """
+    key_actions = [
+        _create_keys
+    ]
+    env.AddPlatformTarget("create_ambiq_keys", None, key_actions, "Create Ambiq Default Keys",
+                          "Create Default Ambiq security keys")
 
 # Removes IDE linting errors :-(
 env = env
@@ -121,4 +174,5 @@ env = env
 add_svl_bootloader(env)
 add_jlink_swo(env)
 add_jlink_rtt(env)
-# add_ota_image(env)
+add_ota_image(env)
+add_create_default_ota_security_keys(env)
